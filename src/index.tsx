dbd883c7a1dcbc8a9dc8cc60e76dd36cd72f4a6e
DevAlberto <devalbertobarboza@gmail.com>
2021-02-14 20:24:32 -0300

    added tailwind, craco, routing and font-awesone

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// layouts
+
+// import Admin from "layouts/Admin.js";
+import Auth from "./layouts/Auth";
+
+// views without layouts
+
+import Home from './views/Home';
+// import Profile from "views/Profile.js";
+// import Index from "views/Index.js";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      {/* add routes with layouts */}
+      {/* <Route path="/admin" component={Admin} /> */}
+      <Route path="/auth" component={Auth} />
+      {/* add routes without layouts */}
+      <Route path="/Home" exact component={Home} />
+      {/* <Route path="/profile" exact component={Profile} /> */}
+      {/* <Route path="/" exact component={Index} /> */}
+      {/* add redirect for first page */}
+      <Redirect from="*" to="/Home" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
